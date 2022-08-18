0ea3132c9a846a6d108fc5d22fdaef5eb224df53
rock12231 <37548802+rock12231@users.noreply.github.com>
2022-08-18 12:24:21 +0530

    whitenoise add in setting and rec file

--- CRUDAjax/settings.py
+++ CRUDAjax/settings.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', #add whitenoise
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
